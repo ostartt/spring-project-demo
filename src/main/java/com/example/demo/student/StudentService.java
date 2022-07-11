475d00cdff2eb00e59dcfd06b093601729ba37e9
Ostap Klymko <oostapk@gmail.com>
2022-07-11 18:11:37 +0300

    Added PUT controller functionality

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,26 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
 
+    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(()-> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"
+                ));
+        if(name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
